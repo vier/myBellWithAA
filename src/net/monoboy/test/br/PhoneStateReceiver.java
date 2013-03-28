008086b9bdd0540b4988c29253d96b6af6e7fc9b
monoboy <vier@paran.com>
2013-03-28 17:28:52 +0900

    smsReceiver 추가

---
+++ src/net/monoboy/test/br/PhoneStateReceiver.java
@@ -0,0 +1,64 @@
+package net.monoboy.test.br;
+
+import net.monoboy.test.RingActivity_;
+
+import com.googlecode.androidannotations.annotations.EReceiver;
+import com.googlecode.androidannotations.annotations.SystemService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.ServiceState;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+@EReceiver
+public class PhoneStateReceiver extends BroadcastReceiver {
+
+	@SystemService
+	TelephonyManager telephonyManager;
+	
+	@Override
+	public void onReceive(Context ctx, Intent intent) {		
+		MyBellPhoneStateListener myBellPhoneStateListener = new MyBellPhoneStateListener();
+		telephonyManager.listen(myBellPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
+		telephonyManager.listen(myBellPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+		
+		Log.d("K5", intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+		
+		if (TelephonyManager.CALL_STATE_RINGING == telephonyManager.getCallState()) {
+			callRingActivity(ctx, intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+		}
+	}
+	
+	private static class MyBellPhoneStateListener extends PhoneStateListener {
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			
+		}
+		
+		@Override
+		public void onServiceStateChanged(ServiceState serviceState) {
+			
+		}
+	}
+
+	private void callRingActivity(final Context ctx, final String incomingPhoneNumber) {
+		final Intent newRingIntent = new Intent(ctx.getApplicationContext(), RingActivity_.class);
+		newRingIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+	
+		new Thread() {
+		    public void run() {
+				try {
+				    sleep(1000);
+				} catch (InterruptedException e) {
+				    e.printStackTrace();
+				} finally {
+				    newRingIntent.putExtra("incoming", incomingPhoneNumber);
+				    ctx.getApplicationContext().startActivity(newRingIntent);
+				}
+		    }
+		}.start();
+	}
+}
---
+++ src/net/monoboy/test/br/SmsReceiver.java
@@ -0,0 +1,48 @@
+package net.monoboy.test.br;
+
+import com.googlecode.androidannotations.annotations.EReceiver;
+import com.googlecode.androidannotations.annotations.UiThread;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+@EReceiver
+public class SmsReceiver extends BroadcastReceiver {
+
+	private static final String SMS_EXTRA_NAME = "pdus";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// Get the SMS map from Intent
+		Bundle extras = intent.getExtras();
+
+		String messages = "";
+
+		if (extras != null) {
+			// Get received SMS array
+			Object[] smsExtra = (Object[]) extras.get(SMS_EXTRA_NAME);
+
+			for (Object aSmsExtra : smsExtra) {
+				SmsMessage sms = SmsMessage.createFromPdu((byte[]) aSmsExtra);
+
+				String body = sms.getMessageBody();
+				String address = sms.getOriginatingAddress();
+
+				messages += "SMS from " + address + " :\n";
+				messages += body + "\n";
+
+				showSMS(context, messages);
+			}
+		}
+	}
+
+	@UiThread
+	public void showSMS(Context context, String messages) {
+		Toast.makeText(context, messages, Toast.LENGTH_SHORT).show();
+	}
+
+}
